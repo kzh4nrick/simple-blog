a2589798505cacfe410b4eb97c148e5756460a76
kzh4nrick <kzh4nrick@users.noreply.github.com>
2025-02-03 14:12:34 +0000

    deploy: 7ac8a93df936a2a7ba978fa01f836d96a841097f

---
+++ _app/immutable/entry/start.CsrArAGu.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/C95YDmS6.js";export{t as start};
---
+++ _app/immutable/nodes/2.DseDTrYE.js
@@ -0,0 +1,1 @@
+import{t as o,a}from"../chunks/B-C5MEyA.js";import"../chunks/C5AbWrXV.js";import{n as p}from"../chunks/-neJYqkk.js";var r=o('<h1>Welcome to SvelteKitt</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=r();p(2),a(t,e)}export{s as component};
